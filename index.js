ad84aff361622fa01215d2861a1877de675bd1b3
Ulyana Sanina <sanina.ulyana111@gmail.com>
2023-12-11 13:34:18 +0400

    add index main file

---
+++ index.js
@@ -0,0 +1,8 @@
+// const Game = require("../one-move-game/game/game");
+
+// const exitOption = "0";
+// const helpOption = "?";
+// const moves = process.argv.slice(2);
+
+// new Game(moves, exitOption, helpOption).startGame();
+
